d5b31b16cbee3e60f1dc50ed7feb0754df70ad17
DevGamorim <pesquisasti.ads@gmail.com>
2023-09-10 14:04:35 -0300

    init screen inicio1

--- Fechadura/App.js
+++ Fechadura/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import inicio1 from './componentes/inicio1';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <inicio1/>
+      <Text>É gatinhah!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ Fechadura/componentes/inicio1.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function inicio1(){
+    return(
+        <Text>Incio 1</Text>
+    )
+};
